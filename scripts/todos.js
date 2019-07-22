f5bc5a576d4a772fc0c0239bcd4ef23e37f396a5
Curtis Crutchfield <crutchc@yahoo.com>
2019-07-22 12:57:54 -0500

    created a navbar in html and added styling with css

---
+++ scripts/todos.js
@@ -0,0 +1,3 @@
+// Fetch and display todos
+
+// These must be displayed in a 4-column grid with the title and a checkbox. If the task is complete, the checkbox must be checked. Only the first 30 tasks should be displayed.
